d296cae93a7d3d690e165d7d3bcd00265a4c62ab
Slava Avdeev <34892700+ProMindSl@users.noreply.github.com>
2018-11-20 11:39:34 +0300

    add js selector for bg li

---
+++ src/blocks/bg-slider/bg-slider.js
@@ -0,0 +1,29 @@
+// BG slider part
+const BG_SL_QTY = 6;
+var currBgSlide = 1;
+
+
+// CURRENT CODE
+document.addEventListener('keydown', (event) => {
+  const keyNum = event.keyCode;
+  if (keyNum === 32)
+  {
+	changeBgSlide();
+  }
+  
+}, false);
+// END CURRENT CODE
+
+
+function changeBgSlide()
+{
+	// убираем предыдущий слайд
+	TweenMax.to($('#bg_id_'+String(currBgSlide)), 0.5, {opacity:0});
+
+	// контроль выхода за предел кол-ва слайдеров
+	currBgSlide++;
+	if (currBgSlide > BG_SL_QTY) currBgSlide = 1;
+
+	// показываем новый слайд
+	TweenMax.to($('#bg_id_'+String(currBgSlide)), 0.5, {opacity:1});
+}
