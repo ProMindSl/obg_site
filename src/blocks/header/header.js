55c9637e816c8f3483f0e982319a5beab406969a
proMind <proMind>
2018-11-23 23:25:33 +0300

    add adaptive for up menu with anim

--- src/blocks/header/header.js
+++ src/blocks/header/header.js
@@ -45,6 +45,7 @@ function getNameByURL(url)
 	{
 		case PAGE_INDEX:  
 		case PAGE_INDEX+'#':
+		case 'http://localhost:3000/':
 			return 'main';
 			break;
 
@@ -67,7 +68,7 @@ function getNameByURL(url)
 
 }
 
-// обработчик клика на кнопку меню
+// обработчик клика на кнопку MAIN меню
 $('#id_head-min-menu').on('click', function() {
 	
 	let _menu = $('.header__menu');
@@ -75,8 +76,35 @@ $('#id_head-min-menu').on('click', function() {
 	if(_menu.css('display') == 'none')	
 	{
 		_menu.css({'display': 'block'});
-		TweenMax.from( _menu, 3, {opacity:0, ease: Power4.easeOut});
+		TweenMax.from( _menu, 0.8, {opacity:0, ease: Power4.easeOut});
 		TweenMax.from( _menu, 0.7, {y:200, ease: Power4.easeOut});
+	}
+	else 
+	{
+		_menu.css({'display': 'none'});
+	}
+});
+
+// обработчик клика на кнопку UP меню
+$('.header__up-bar__btn-show-up-menu').on('click', function() {
+	
+	let _menu = $('.header__up-bar__up-menu');
+	
+	if(_menu.css('display') == 'none')	
+	{
+		_menu.css({'display': 'inline-block'});
+		
+		// Анимация кнопок меню
+		let _btn_qty = 3; // кол-во кнопок
+		for(let btn_nums=0; btn_nums<_btn_qty; btn_nums++)
+		{
+			TweenMax.from( $('.header__up-bar__up-menu__btn[name="btn-'+String(btn_nums+1)+'"]'), 
+						   0.8, 
+						   {opacity:0, delay: Number((btn_nums+1)/10), ease: Power4.easeOut});
+			TweenMax.from( $('.header__up-bar__up-menu__btn[name="btn-'+String(btn_nums+1)+'"]'), 
+						   0.7, 
+						   {y:200, delay: Number((btn_nums+1)/10), ease: Power4.easeOut});
+		}
 	}
 	else 
 	{
@@ -89,8 +117,9 @@ $('#id_head-min-menu').on('click', function() {
 function windowResizeHandler_fromHeader()
 {
 	let _menu = $('.header__menu');
+	let _up_menu = $('.header__up-bar__up-menu');
 
-
+	// для main menu
 	if ($(window).width() > '600')
 	{
 		if(_menu.css('display') == 'none')	_menu.css({'display': 'inline-block'});
@@ -98,6 +127,16 @@ function windowResizeHandler_fromHeader()
 	else if ($(window).width() <= '600')
 	{
 		_menu.css({'display': 'none'});
+	} 
+
+	// для up menu
+	if ($(window).width() > '1050')
+	{
+		if(_up_menu.css('display') == 'none')	_up_menu.css({'display': 'inline-block'});
+	}
+	else if ($(window).width() <= '1050')
+	{
+		_up_menu.css({'display': 'none'});
 	} 
 }
 $(window).resize(windowResizeHandler_fromHeader); // при изменении размеров
